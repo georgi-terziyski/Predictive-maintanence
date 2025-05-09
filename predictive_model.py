ba11f601a0c0773ed2ecb7003a3f24c60e098257
alexgeorgievhpe <alex.georgiev@hpe.com>
2025-05-09 14:01:20 +0300

    changed matplotlib to use Agg insted of TkAgg for VM reasons

--- predictive_model.py
+++ predictive_model.py
@@ -1,6 +1,7 @@
 # predictive_model.py (FINAL VERSION - Incorporating all fixes)
 
-import matplotlib; matplotlib.use('TkAgg')
+import matplotlib; matplotlib.use('Agg')
+# import matplotlib; matplotlib.use('TkAgg')
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split 
